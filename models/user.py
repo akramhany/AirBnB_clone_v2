4b266d518f6eb7e442e5be0051bf5c2b9d9d7c12
amir-kedis <amir.kedis@gmail.com>
2024-04-17 21:55:24 +0200

    feat: user.pu

--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This module defines a class User."""
+from sqlalchemy import Column, String
+
+import models
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """This class defines a user by various attributes."""
+
+    if models.storage_type == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
