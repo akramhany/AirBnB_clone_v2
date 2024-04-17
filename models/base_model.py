d69a7d28a025a44a09ca7e5b68981237e057b6cb
amir-kedis <amir.kedis@gmail.com>
2024-04-17 17:49:35 +0200

    chore: base_model formating changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""This module defines a base class for all models in our hbnb clone."""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for all hbnb models."""
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instatntiate a new model."""
         if not kwargs:
             from models import storage
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            del kwargs["__class__"]
             self.__dict__.update(kwargs)
 
     def add_attr(self, attr_name, attr_value):
-        """ Takes an attribute name and a value and updates it """
+        """Take an attribute name and a value and updates it."""
         try:
-            if attr_value[0] == "\"" and attr_value[-1] == "\"":
-                st = attr_value.strip("\"")
+            if attr_value[0] == '"' and attr_value[-1] == '"':
+                st = attr_value.strip('"')
                 argu = ""
                 sp = ""
-                for word in st.split('_'):
+                for word in st.split("_"):
                     word = sp + word
                     argu += word
                     sp = " "
@@ -43,22 +46,22 @@ class BaseModel:
             self.__dict__[attr_name] = str(attr_value)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """Return a string representation of the instance."""
+        cls = (str(type(self)).split(".")[-1]).split("'")[0]
+        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Update updated_at with current time when instance is changed."""
         from models import storage
+
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
+        """Convert instance into dict format."""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
